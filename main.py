5f306dbeb5b32edad64ddadef73f98ad63d877b0
somm-kim <somm.kim@navercorp.com>
2024-07-25 00:32:50 +0900

    결제까지 성공

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+import time
 import yaml
 import logging
 import argparse
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--log-level', default='INFO')
+parser.add_argument('--headless', default=False)
 args = parser.parse_args()
+
+
+def get_element(element, key, value, timeout=10):
+    WebDriverWait(element, timeout).until(EC.presence_of_element_located((key, value)))
+    return element.find_element(key, value)
+
+def get_elements(element, key, value, timeout=10):
+    WebDriverWait(element, timeout).until(EC.presence_of_element_located((key, value)))
+    return element.find_elements(key, value)
+
+def click_button(element, key, value, timeout=10):
+    # element 클릭이 이루어지려면 "시각적으로 보여야함"
+    btn = WebDriverWait(element, timeout).until(EC.element_to_be_clickable((key, value)))
+    btn.click()
+
+def check_box(element, key, value, timeout=10):
+    checkbox = get_element(element, key, value, timeout)
+    driver.execute_script("arguments[0].click();", checkbox)
 
 LOG_LEVEL = {
     'DEBUG': 10,
@@ -33,27 +53,29 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # config 파일 읽기
-with open('config.yaml', 'r') as f:
+with open('config.yaml', 'r', encoding='utf-8') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-ID = config['id']
-PASSWORD = config['password']
+# course config 파일 읽기
+with open('course.yaml', 'r', encoding='utf-8') as f:
+    course = yaml.load(f, Loader=yaml.FullLoader)
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('headless')
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-driver = webdriver.Chrome(service=Service(
-    ChromeDriverManager().install()), options=chrome_options)
+if args.headless:
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=chrome_options)
+else:
+    driver = webdriver.Chrome()
 
 # 로그인 화면
 driver.get(url='https://spo.isdc.co.kr/login.do')
 
 # ID, PASSWORD 입력
-element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "mbId")))
-element.send_keys(ID)
-element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "mbPw")))
-element.send_keys(PASSWORD)
+get_element(driver, By.ID, 'mbId').send_keys(config['id'])
+get_element(driver, By.ID, 'mbPw').send_keys(config['password'])
 
 # 로그인 버튼 스크립트 동작
 driver.execute_script('login()')
@@ -70,52 +92,109 @@ logger.debug('Login succeed')
 # 수강신청 화면 이동
 driver.get(url='https://spo.isdc.co.kr/courseRegist.do')
 
-select = Select(driver.find_element(By.ID, 'center'))
-select.select_by_visible_text('탄천종합운동장')
+# 강좌 선택
+my_course = course['seongnam_swim']
 
-select = Select(driver.find_element(By.ID, 'event'))
-select.select_by_visible_text('50M기간수료제수영')
+select = Select(get_element(driver, By.ID, 'center'))
+select.select_by_visible_text(my_course['center'])
 
-select = Select(driver.find_element(By.ID, 'class'))
-select.select_by_visible_text('기간제_연수반')
+select = Select(get_element(driver, By.ID, 'event'))
+select.select_by_visible_text(my_course['event'])
 
-select = Select(driver.find_element(By.ID, 'target'))
-select.select_by_visible_text('성인')
+select = Select(get_element(driver, By.ID, 'class'))
+select.select_by_visible_text(my_course['class'])
 
-# 강좌명 입력
-pgm_nm = driver.find_element(By.ID, 'pgm_nm')
-pgm_nm.send_keys('[07시] 기간수료_연수반_주5회')
+select = Select(get_element(driver, By.ID, 'target'))
+select.select_by_visible_text(my_course['target'])
+
+get_element(driver, By.ID, 'pgm_nm').send_keys(my_course['class_name'])
 
 # 조회 버튼 클릭
-driver.find_element(By.ID, 'submit').click()
+click_button(driver, By.ID, 'submit', 10)
 
 # table 뜰 때까지 기다림.
 logger.debug('Waiting table')
-element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "table_list_info")))
+get_element(driver, By.ID, 'table_list_info', 10)
 
-# 대기 신청 버튼 찾기
-logger.debug('Waiting waitreg button')
-element = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "waitreg")))
-waitreg_button = driver.find_element(By.NAME, 'waitreg')
+# 신청 버튼 클릭
+click_button(driver, By.NAME, 'acs', 10)
 
-# 대기신청 버튼 클릭
-logger.debug('Click waitreg button')
-waitreg_button.click()
-
-# '등록하시겠습니까' alert 창으로 이동해서 확인 버튼 누름.
+# '신청목록 장바구니로 가시겠습니까' alert 창으로 이동해서 확인 버튼 누름.
 try:
     alert = driver.switch_to.alert
     alert.accept()
 except Exception as e:
     raise e
 
-# '등록되었습니다' alert 창으로 이동해서 확인 버튼 누름.
+# 장바구니 화면
+driver.get(url='https://spo.isdc.co.kr/courseCart.do')
+click_button(driver, By.NAME, 'cart_check')
+
+select = Select(get_element(driver, By.CLASS_NAME, 'qty'))
+select.select_by_visible_text('1 개월')
+
+select = Select(get_element(driver, By.CLASS_NAME, 'dc_sel'))
+select.select_by_visible_text('할인없음')   # 여성할인
+
+# 강좌신청하기 버튼 클릭
+click_button(driver, By.ID, 'order')
+
+# '주문하기로 이동하시겠습니까' alert 창으로 이동해서 확인 버튼 누름.
 try:
     alert = driver.switch_to.alert
     alert.accept()
-    logger.info('Success !!!')
 except Exception as e:
     raise e
+
+# 결제하기 버튼 클릭
+click_button(driver, By.CLASS_NAME, 'blue')
+
+# 현재 창의 핸들을 저장
+main_window = driver.current_window_handle
+
+# 새창으로 이동
+for handle in driver.window_handles:
+    if handle != main_window:
+        driver.switch_to.window(handle)
+        break
+
+# 지불 방법을 가상계좌로 변경
+element = get_element(driver, By.NAME, 'pay_method')
+select = Select(element)
+select.select_by_visible_text('가상계좌')
+
+# 결제요청 버튼 클릭
+click_button(driver, By.CLASS_NAME, 'submit')
+
+# iframe을 감싸고 있는 div가 뜰 때까지 대기
+element = get_element(driver, By.CLASS_NAME, "blockUI")
+
+# iframe 요소로 전환
+iframe = get_element(driver, By.ID, "naxIfr")
+driver.switch_to.frame(iframe)
+
+# 전체동의 클릭
+check_box(driver, By.ID, 'chk_all')
+
+# 입금은행 선택
+select = Select(get_element(driver, By.ID, 'select_bank'))
+select.select_by_visible_text('KB국민은행')
+
+# 다음 버튼이 안보여서 크기 조절
+width = driver.get_window_size().get('width')
+driver.set_window_size(width, 800)
+
+# 다음 버튼 클릭
+click_button(driver, By.ID, 'spayNext')
+
+# 현금영수증 발행 체크 해제
+check_box(driver, By.ID, 'chk_num2')
+
+# 결제요청 버튼 클릭
+click_button(driver, By.ID, 'spayNext')
+
+# 데이터가 전송되기까지 기다려줘야함.
+time.sleep(5)
 
 # WebDriver 종료
 driver.quit()
